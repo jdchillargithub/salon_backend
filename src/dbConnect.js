af58965ca1ba1ae446d2d7ede81baa79049d3862
jdchillar <jithin.das@chillarcards.com>
2024-08-01 12:03:20 +0530

    changes added

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -1,7 +1,7 @@
 import currentConfig from '../config.js'
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('doctorapp', process.env.MYSQL_USER, '', {
+const sequelize = new Sequelize('salonapp', process.env.MYSQL_USER, '', {
     dialect: 'mysql',
     // host: 'localhost',
     host: process.env.MYSQL_HOST,
@@ -10,6 +10,10 @@ const sequelize = new Sequelize('doctorapp', process.env.MYSQL_USER, '', {
     password: process.env.MYSQL_PASSWORD,
     // database: currentConfig.MYSQL_DATABASE,
     logging: false, // Set to true to log SQL queries (optional)
+    define: {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci'
+    },
 });
 
 // const sequelize = new Sequelize('platform_doctor', 'root', '', {
